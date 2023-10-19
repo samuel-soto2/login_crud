b9fd0b31bc7a6bacd49148152cdc91b2acf96f32
samuel-soto2 <1461888@senati.pe>
2023-10-19 17:10:42 -0500

    realizamos los distintos campos a completar

---
+++ app/Models/User.php
@@ -0,0 +1,22 @@
+
+
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
