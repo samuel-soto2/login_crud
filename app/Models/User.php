a87ee17dd543824343abf1623a4b6a1af9b2776e
samuel-soto2 <1461888@senati.pe>
2023-10-19 17:13:56 -0500

    correcion para poder rellenar los campos

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,21 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
 
 
-/**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
@@ -20,3 +35,18 @@
         'password',
         'remember_token',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+    public function fromDateTime($value)
+    {
+        return Carbon::parse(parent::fromDateTime($value))->format('Y-d-m H:i:s');
+    }
+}
